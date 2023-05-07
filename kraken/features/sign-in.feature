84ae2154d11474d46ba7fc3ae18fa0551f835e13
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-05-06 22:43:52 -0500

    Creando pasos para el SignIn - validando que inicie session.

--- kraken/features/sign-in.feature
+++ kraken/features/sign-in.feature
@@ -2,6 +2,11 @@ Feature: Sign In
 
 @user2 @web
 Scenario: Iniciar sesión en el dashboard administrativo con un email y password existente y validos
-  Given I navigate to page "https://outlook.com"
+  Given I navigate to page "http://localhost:2368/ghost/#/signin"
+  And I wait for 5 seconds
+  And I enter email "daniel.jimenez@ingenieros.com"
+  And I enter password "Asdf1234$*"
+  And I click on the button Sing in ->
+  And I wait for 5 seconds
+  Then I should be logged in
   And I wait for 2 seconds
-  And I enter email "test@test.com"
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,6 +1,29 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 When('I enter email {string}', async function (email) {
-    let element = await this.driver.$('input');
-    return await element.setValue(email);
+    let emailField = await this.driver.$('#identification');
+    return await emailField.setValue(email);
 });
+
+
+When('I enter password {string}', async function(pass){
+    let passField = await this.driver.$('#password');
+    return await passField.setValue(pass);
+});
+
+When('I click on the button Sing in ->', async function () {
+    let button = await this.driver.$(`#ember5`);
+    return await button.click();
+  });
+  
+  Then('I should be logged in', async function () {
+      
+    let welcomeMessageElement = await this.driver.$('.gh-dashboard-zero-message h4');
+    let welcomeMessage = await welcomeMessageElement.getText();
+  
+    if (welcomeMessage.includes('Welcome')) {
+      console.log('Inicio de sesión exitoso');
+    } else {
+        throw new Error('Fallo en el inicio de sesión');
+    }
+  });
