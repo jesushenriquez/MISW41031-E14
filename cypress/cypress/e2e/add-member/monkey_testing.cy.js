dacc974a5741a152b6f5c74e088db640bad03ab2
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-07 21:46:39 -0500

    Members test cases running

--- cypress/cypress/e2e/add-member/monkey_testing.cy.js
+++ cypress/cypress/e2e/add-member/monkey_testing.cy.js
@@ -21,4 +21,36 @@ describe('Create members', () => {
         cy.wait(1000);
         cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
     })
+
+    it('Test create member just using email', () => {
+        signIn();
+        cy.get('a[href="#/members/"]').click();
+        cy.wait(1000);
+        cy.get('a[href="#/members/new/"]').click();	
+        cy.wait(1000);
+        cy.get('#member-email').type(faker.internet.email());
+        cy.wait(1000);
+        cy.get('button[data-test-button="save"]').click();
+        cy.wait(2000);
+        cy.get('a[data-test-link="members-back"]').click();
+        cy.wait(1000);
+        cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("")').should('have.length.at.least', 1);
+    })
+    
+    it('Test create member without note field', () => {
+        signIn();
+        cy.get('a[href="#/members/"]').click();
+        cy.wait(1000);
+        cy.get('a[href="#/members/new/"]').click();	
+        cy.wait(1000);
+        cy.get('#member-name').type('Cypress member name');
+        cy.wait(1000);
+        cy.get('#member-email').type(faker.internet.email());
+        cy.wait(1000);
+        cy.get('button[data-test-button="save"]').click();
+        cy.wait(2000);
+        cy.get('a[data-test-link="members-back"]').click();
+        cy.wait(1000);
+        cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
+    });
 })
