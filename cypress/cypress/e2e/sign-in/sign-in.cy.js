ef1d44ea800e88e9d0b9351bb9d9c77b794ac646
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-05-07 22:58:21 -0500

    Ajustes de forma y habilitando test sign-in negativo

--- cypress/cypress/e2e/sign-in/sign-in.cy.js
+++ cypress/cypress/e2e/sign-in/sign-in.cy.js
@@ -3,7 +3,7 @@ describe('Sign-in', function() {
       cy.fixture('sign-in.json').as('loginData');
     });
   
-   /*  it('Intento de inicio de sesion fallido', function() {
+    it('Intento de inicio de sesion fallido', function() {
       cy.visit('http://localhost:2368/ghost/#/signin');
       cy.wait(1000);
       cy.get('#identification').type('email@Mal.com');
@@ -12,7 +12,7 @@ describe('Sign-in', function() {
       cy.wait(1000);
       cy.get('#ember5').contains('Retry');
       cy.wait(1000);
-    }); */
+    });
   
     it('Iniciar sesión en el dashboard administrativo con un email y password existente y válidos', function() {
       cy.get('@loginData').then((loginData) => {
