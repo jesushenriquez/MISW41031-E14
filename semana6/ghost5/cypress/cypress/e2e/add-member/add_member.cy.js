d4ad8ab755783f35597c46100d6dd98e1712d2df
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-09 23:22:05 -0500

    Change screenshot order

--- semana6/ghost5/cypress/cypress/e2e/add-member/add_member.cy.js
+++ semana6/ghost5/cypress/cypress/e2e/add-member/add_member.cy.js
@@ -8,33 +8,32 @@ describe('Create members', () => {
         signIn();
         screen("Paso 1");
         cy.get('a[href="#/members/"]').click();
-        screen("Paso 2");
         cy.wait(1000);
+        screen("Paso 2");
         cy.get('a[href="#/members/new/"]').its('length').then((length) => {
             if (length === 1) {
                 cy.get('a[href="#/members/new/"]').click()
-                screen("Paso 3");
             } else {
                 cy.get('a[href="#/members/new/"]').first().click()
-                screen("Paso 3");
             }
         });	
         cy.wait(1000);
+        screen("Paso 3");
         cy.get('#member-name').type('Cypress member name');
+        cy.wait(1000);
         screen("Paso 4");
-        cy.wait(1000);
         cy.get('#member-email').type(faker.internet.email());
+        cy.wait(1000);
         screen("Paso 5");
-        cy.wait(1000);
         cy.get('#member-note').type('Cypress member note');
+        cy.wait(1000);
         screen("Paso 6");
-        cy.wait(1000);
         cy.get('button[data-test-button="save"]').click();
-        screen("Paso 7");
         cy.wait(2000);
+        screen("Paso 7");
         cy.get('a[data-test-link="members-back"]').click();
-        screen("Paso 8");
         cy.wait(1000);
+        screen("Paso 8");
         cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
         screen("Paso 9");
     })
@@ -44,20 +43,20 @@ describe('Create members', () => {
         signIn();
         screen("Paso 1");
         cy.get('a[href="#/members/"]').click();
+        cy.wait(1000);
         screen("Paso 2");
+        cy.get('a[href="#/members/new/"]').click();	
         cy.wait(1000);
-        cy.get('a[href="#/members/new/"]').click();
-        screen("Paso 3");	
-        cy.wait(1000);
+        screen("Paso 3");
         cy.get('#member-email').type(faker.internet.email());
+        cy.wait(1000);
         screen("Paso 4");
-        cy.wait(1000);
         cy.get('button[data-test-button="save"]').click();
-        screen("Paso 5");
         cy.wait(2000);
+        screen("Paso 5");
         cy.get('a[data-test-link="members-back"]').click();
-        screen("Paso 6");
         cy.wait(1000);
+        screen("Paso 6");
         cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("")').should('have.length.at.least', 1);
         screen("Paso 7");
     })
@@ -65,25 +64,26 @@ describe('Create members', () => {
     it('Test create member without note field', () => {
         let screen = screenshot.bind(null, "Add Members", "Crear un nuevo miembro desde el panel de members sin usar el campo nota");
         signIn();
-        cy.get('a[href="#/members/"]').click();
         screen("Paso 1");
+        cy.get('a[href="#/members/"]').click();
         cy.wait(1000);
-        cy.get('a[href="#/members/new/"]').click();
-        screen("Paso 2");	
+        screen("Paso 2");
+        cy.get('a[href="#/members/new/"]').click();	
         cy.wait(1000);
-        cy.get('#member-name').type('Cypress member name');
         screen("Paso 3");
+        cy.get('#member-name').type('Cypress member name');
         cy.wait(1000);
-        cy.get('#member-email').type(faker.internet.email());
         screen("Paso 4");
+        cy.get('#member-email').type(faker.internet.email());
         cy.wait(1000);
-        cy.get('button[data-test-button="save"]').click();
         screen("Paso 5");
+        cy.get('button[data-test-button="save"]').click();
         cy.wait(2000);
-        cy.get('a[data-test-link="members-back"]').click();
         screen("Paso 6");
+        cy.get('a[data-test-link="members-back"]').click();
         cy.wait(1000);
-        cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
         screen("Paso 7");
+        cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
+        screen("Paso 8");
     });
 })
