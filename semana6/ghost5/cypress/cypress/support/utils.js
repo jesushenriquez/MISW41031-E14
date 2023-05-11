30b4b46f1dd2037163e3e3e2495eac92b14feebe
Daniel Jimenez <d.jimenez112@uniandes.edu.co>
2023-05-11 16:46:52 -0500

    Ajustando pruebas para Ghost3

--- semana6/ghost5/cypress/cypress/support/utils.js
+++ semana6/ghost5/cypress/cypress/support/utils.js
@@ -1,6 +1,5 @@
 function signIn() {
     cy.visit('http://localhost:2368/ghost/#/signin');
-    cy.wait(1000);
     cy.get('#identification').type("test@test.com");
     cy.get('#password').type("pnz5nrp.put7jxt_DCH");
     cy.get('#ember5').click();
