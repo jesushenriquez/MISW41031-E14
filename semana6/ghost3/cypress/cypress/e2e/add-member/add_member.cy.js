f39093324232563bf0f3d9567466e7254bce3766
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-13 14:57:01 -0500

    Test cases running

--- semana6/ghost3/cypress/cypress/e2e/add-member/add_member.cy.js
+++ semana6/ghost3/cypress/cypress/e2e/add-member/add_member.cy.js
@@ -1,19 +1,20 @@
 import { faker } from '@faker-js/faker';
 
-const { signIn } = require('../../support/utils');
+const { signIn, MembersActivation } = require('../../support/utils');
 
 describe('Create members', () => {
     it('Test create member', () => {
         signIn();
-        cy.get('a[href="#/members/"]').click();
         cy.wait(1000);
-        cy.get('a[href="#/members/new/"]').its('length').then((length) => {
+        cy.get('a[href="#/members/"').click();
+        cy.wait(1000);
+        cy.get('a[href="#/members/new/"').its('length').then((length) => {
             if (length === 1) {
-                cy.get('a[href="#/members/new/"]').click()
+                cy.get('a[href="#/members/new/"').click()
             } else {
-                cy.get('a[href="#/members/new/"]').first().click()
+                cy.get('a[href="#/members/new/"').first().click()
             }
-        });
+        });	
         cy.wait(1000);
         cy.get('#member-name').type('Cypress member name');
         cy.wait(1000);
@@ -21,41 +22,33 @@ describe('Create members', () => {
         cy.wait(1000);
         cy.get('#member-note').type('Cypress member note');
         cy.wait(1000);
-        cy.get('button[data-test-button="save"]').click();
-        cy.wait(2000);
-        cy.get('a[data-test-link="members-back"]').click();
+        cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').contains('Save').click();
+        cy.wait(1000);
+        cy.get('a[href="#/members/"').first().click();
         cy.wait(1000);
         cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
     })
 
-    it('Test create member just using email', () => {
-        signIn();
-        cy.get('a[href="#/members/"]').click();
-        cy.wait(1000);
-        cy.get('a[href="#/members/new/"]').click();	
-        cy.wait(1000);
-        cy.get('#member-email').type(faker.internet.email());
-        cy.wait(1000);
-        cy.get('button[data-test-button="save"]').click();
-        cy.wait(2000);
-        cy.get('a[data-test-link="members-back"]').click();
-        cy.wait(1000);
-        cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("")').should('have.length.at.least', 1);
-    })
-    
     it('Test create member without note field', () => {
         signIn();
-        cy.get('a[href="#/members/"]').click();
         cy.wait(1000);
-        cy.get('a[href="#/members/new/"]').click();	
+        cy.get('a[href="#/members/"').click();
+        cy.wait(1000);
+        cy.get('a[href="#/members/new/"').its('length').then((length) => {
+            if (length === 1) {
+                cy.get('a[href="#/members/new/"').click()
+            } else {
+                cy.get('a[href="#/members/new/"').first().click()
+            }
+        });	
         cy.wait(1000);
         cy.get('#member-name').type('Cypress member name');
         cy.wait(1000);
         cy.get('#member-email').type(faker.internet.email());
         cy.wait(1000);
-        cy.get('button[data-test-button="save"]').click();
-        cy.wait(2000);
-        cy.get('a[data-test-link="members-back"]').click();
+        cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').contains('Save').click();
+        cy.wait(1000);
+        cy.get('a[href="#/members/"').first().click();
         cy.wait(1000);
         cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
     });
