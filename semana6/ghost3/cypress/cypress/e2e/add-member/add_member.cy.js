0335ac1aba09d1cdc3829bebc500affe52028c23
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-13 15:07:07 -0500

    Test case with screenshots

--- semana6/ghost3/cypress/cypress/e2e/add-member/add_member.cy.js
+++ semana6/ghost3/cypress/cypress/e2e/add-member/add_member.cy.js
@@ -40,10 +40,13 @@ describe('Create members', () => {
     })
 
     it('Test create member without note field', () => {
+        let screen = screenshot.bind(null, "Add Members", "Crear un nuevo miembro desde el panel de members sin usar el campo nota");
         signIn();
         cy.wait(1000);
+        screen("Paso 1");
         cy.get('a[href="#/members/"').click();
         cy.wait(1000);
+        screen("Paso 2");
         cy.get('a[href="#/members/new/"').its('length').then((length) => {
             if (length === 1) {
                 cy.get('a[href="#/members/new/"').click()
@@ -52,14 +55,20 @@ describe('Create members', () => {
             }
         });	
         cy.wait(1000);
+        screen("Paso 3");
         cy.get('#member-name').type('Cypress member name');
         cy.wait(1000);
+        screen("Paso 4");
         cy.get('#member-email').type(faker.internet.email());
         cy.wait(1000);
+        screen("Paso 5");
         cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').contains('Save').click();
         cy.wait(1000);
+        screen("Paso 6");
         cy.get('a[href="#/members/"').first().click();
         cy.wait(1000);
+        screen("Paso 7");
         cy.get('h3.gh-members-list-name ', { timeout: 10000 }).filter(':contains("Cypress member name")').should('have.length.at.least', 1);
+        screen("Paso 8");
     });
 })
