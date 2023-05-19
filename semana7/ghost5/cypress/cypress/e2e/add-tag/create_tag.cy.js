2503af3f9826cf75bd1981b234a7b1c2bad305d5
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-19 17:49:08 -0500

    Remove screens

--- semana7/ghost5/cypress/cypress/e2e/add-tag/create_tag.cy.js
+++ semana7/ghost5/cypress/cypress/e2e/add-tag/create_tag.cy.js
@@ -1,12 +1,10 @@
-const { signIn, screenshot } = require('../../support/utils');
+const { signIn } = require('../../support/utils');
 
 describe('Add Tags', function() {
     it('Editar información de un tag existente', function() {
-        let screen = screenshot.bind(null, "Add Tags", "Edit existing tag information");
         signIn();
         cy.fixture('tags.json').then((tags)=>{
             for (let index = 0; index < 10; index++) {
-                screen("Paso 1");
                 cy.get('a[href="#/tags/"].ember-view').its('length').then((length) => {
                     if (length === 1) {
                         cy.get('a[href="#/tags/"]').click();
@@ -15,57 +13,43 @@ describe('Add Tags', function() {
                     }
                 });
                 cy.wait(1000);
-                screen("Paso 2");
 
                 cy.get('a[title="Edit tag"').last().click();
                 cy.wait(1000);
-                screen("Paso 3");
                 
                 cy.get('#tag-name').clear();
                 cy.wait(1000);
-                screen("Paso 4");
 
                 cy.get('#tag-name').type(tags[index].name);
                 cy.wait(1000);
-                screen("Paso 5");
 
                 cy.get('#tag-slug').clear();
                 cy.wait(1000);
-                screen("Paso 6");
-
+                
                 cy.get('#tag-slug').type(tags[index].slug);
                 cy.wait(1000);
-                screen("Paso 7");
-
+                
                 cy.get('#tag-description').clear();
                 cy.wait(1000);
-                screen("Paso 9");
-
+                
                 cy.get('#tag-description').type(tags[index].description);
                 cy.wait(1000);
-                screen("Paso 9");
-
+                
                 cy.get('button[data-test-button="save"]').click();
                 cy.wait(1000);
-                screen("Paso 10");
-
                 
                 cy.get('a[href="#/tags/"].ember-view').first().click();
                 cy.wait(1000);
-                screen("Paso 11");
 
                 cy.get('h3.gh-tag-list-name', { timeout: 10000 }).filter(`:contains(${tags[index].name})`).should('have.length.at.least', 1);
-                screen("Paso 12");
             }
         })
     })
 
-    it.only('Editar información de un tag existente dejando campos vacios', function() {
-        let screen = screenshot.bind(null, "Add Tags", "Edit existing tag information leaving empty fields");
+    it('Editar información de un tag existente dejando campos vacios', function() {
         signIn();
         cy.fixture('tags.json').then((tags)=>{
             for (let index = 11; index < 21; index++) {
-                screen("Paso 1");
                 cy.get('a[href="#/tags/"].ember-view').its('length').then((length) => {
                     if (length === 1) {
                         cy.get('a[href="#/tags/"]').click();
@@ -74,39 +58,29 @@ describe('Add Tags', function() {
                     }
                 });
                 cy.wait(1000);
-                screen("Paso 2");
-
+                
                 cy.get('a[title="Edit tag"').last().click();
                 cy.wait(1000);
-                screen("Paso 3");
                 
                 cy.get('#tag-name').clear();
                 cy.wait(1000);
-                screen("Paso 4");
-
+                
                 cy.get('#tag-name').type(tags[index].name);
                 cy.wait(1000);
-                screen("Paso 5");
-
+                
                 cy.get('#tag-slug').clear();
                 cy.wait(1000);
-                screen("Paso 6");
-
+                
                 cy.get('#tag-description').clear();
                 cy.wait(1000);
-                screen("Paso 9");
-
+                
                 cy.get('button[data-test-button="save"]').click();
                 cy.wait(1000);
-                screen("Paso 10");
-
                 
                 cy.get('a[href="#/tags/"].ember-view').first().click();
                 cy.wait(1000);
-                screen("Paso 11");
-
+                
                 cy.get('h3.gh-tag-list-name', { timeout: 10000 }).filter(`:contains(${tags[index].name})`).should('have.length.at.least', 1);
-                screen("Paso 12");
             }
         })
     })
