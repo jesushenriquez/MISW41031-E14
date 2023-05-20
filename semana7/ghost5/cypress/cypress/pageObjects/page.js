412e0d5c1616c852f84559cf0d1484b37a3d1e9b
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-19 20:20:28 -0500

    Edit existing page information

--- semana7/ghost5/cypress/cypress/pageObjects/page.js
+++ semana7/ghost5/cypress/cypress/pageObjects/page.js
@@ -1,19 +1,43 @@
 class Page {
+  clickFirstPageLink() {
+    cy.get('a[data-test-nav="pages"]').first().click();
+  }
+
   clickPageLink() {
     cy.get('a[data-test-nav="pages"]').click();
   }
+
+  clickFirstPagesListElement(){
+    cy.get('a[class="ember-view permalink gh-list-data gh-post-list-title"]').first().click();
+  }
+
   clickNavigateToPageEditor() {
     cy.get('a[href="#/editor/page/"]').click();
   }
+
+  clearTitle(){
+    cy.get('textarea[placeholder="Page title"]').clear();
+  }
+
   title(text) {
     cy.get('textarea[placeholder="Page title"]').type(text);
   }
+
+  typeDescription(description){
+    cy.get('div[data-placeholder="Begin writing your page..."]').type(description);
+  }
+
+  clearDescription(){
+    cy.get('div[data-placeholder="Begin writing your page..."]').clear();
+  }
+
   type(value) {
     cy.get("div.koenig-editor__editor-wrapper > div").last().scrollIntoView();
     cy.get("div.koenig-editor__editor-wrapper > div")
       .last()
       .type(`${value}{enter}`, { force: true });
   }
+
   publishAndBackToEditor() {
     cy.get('button[data-test-button="publish-flow"]').click();
     cy.get('button[data-test-button="continue"]').click();
@@ -22,15 +46,22 @@ class Page {
 
   }
 
-  gotoPagesList(){
-            cy.get('a[data-test-link="pages"]').click();
+  publishUpdate(){
+    cy.get('button[data-test-button="publish-save"]').click();
+  }
 
+  gotoPagesList(){
+    cy.get('a[data-test-link="pages"]').click();
+  }
+
+  clickNavigationPage(){
+    cy.get('a[href="#/pages/"]').click();
   }
 
   checkTitleInList(title){
-            cy.get("h3.gh-content-entry-title")
-              .filter(`:contains("${title}")`)
-              .should("have.length.at.least", 1);
+    cy.get("h3.gh-content-entry-title")
+      .filter(`:contains("${title}")`)
+      .should("have.length.at.least", 1);
   }
 }
 
