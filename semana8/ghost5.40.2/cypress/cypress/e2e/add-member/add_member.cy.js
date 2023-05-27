298f556795ed017426c21aa5c6f69bea82c7c287
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-05-27 12:45:33 -0500

    Add screens to to members

--- semana8/ghost5.40.2/cypress/cypress/e2e/add-member/add_member.cy.js
+++ semana8/ghost5.40.2/cypress/cypress/e2e/add-member/add_member.cy.js
@@ -1,14 +1,14 @@
-const { signIn, readUsersData } = require('../../support/utils');
+const { signIn, readUsersData, screenshot } = require('../../support/utils');
 const {Member} = require("../../pageObjects/member");
 
 describe('Create members', () => {
-    
     const member = new Member();
 
     it('Test create member', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 0; index < 3; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test create member ${index}`);
                 cy.get('a[href="#/members/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickMemberLink();
@@ -17,6 +17,7 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso1");
 
                 cy.get('a[href="#/members/new/"]').its('length').then((length) => {
                     if (length === 1) {
@@ -26,23 +27,30 @@ describe('Create members', () => {
                     }
                 });	
                 cy.wait(1000);
-                
+                screen("paso2");
+
                 member.typeName(users[index].Displayname);
                 cy.wait(1000);
-                
+                screen("paso3");
+
                 member.typeEmail(users[index].Username);
                 cy.wait(1000);
-                
+                screen("paso3");
+
                 member.typeNote(users[index].Department);
                 cy.wait(1000);
-                
+                screen("paso4");
+
                 member.saveCreation();
                 cy.wait(2000);
-                
+                screen("paso5");
+
                 member.returnMembersList();
                 cy.wait(1000);
-                
+                screen("paso6");
+
                 member.checkNameInList(users[index].Displayname);
+                screen("paso7");
             }
         })
     })
@@ -51,6 +59,7 @@ describe('Create members', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 10; index < 13; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test create member without name ${index}`);
                 cy.get('a[href="#/members/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickMemberLink();
@@ -59,6 +68,8 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso1");
+
                 cy.get('a[href="#/members/new/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickNewMemberLink();
@@ -67,20 +78,27 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso2");
 
                 member.typeEmail(users[index].Username);
                 cy.wait(1000);
+                screen("paso3");
                 
                 member.typeNote(users[index].Department);
                 cy.wait(1000);
-                
+                screen("paso4");
+
+
                 member.saveCreation();
                 cy.wait(2000);
+                screen("paso5");
                 
                 member.returnMembersList();
                 cy.wait(1000);
-                
+                screen("paso6");
+
                 member.checkEmptyStringNameInList();
+                screen("paso7");
             }
         })
     })
@@ -89,6 +107,8 @@ describe('Create members', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 20; index < 23; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test create member without note ${index}`);
+
                 cy.get('a[href="#/members/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickMemberLink();
@@ -97,6 +117,9 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso1");
+
+
                 cy.get('a[href="#/members/new/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickNewMemberLink();
@@ -105,20 +128,26 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso2");
 
                 member.typeName(users[index].Displayname);
                 cy.wait(1000);
+                screen("paso3");
 
                 member.typeEmail(users[index].Username);
                 cy.wait(1000);
+                screen("paso4");
                 
                 member.saveCreation();
                 cy.wait(2000);
+                screen("paso5");
                 
                 member.returnMembersList();
                 cy.wait(1000);
+                screen("paso6");
                 
                 member.checkNameInList(users[index].Displayname);
+                screen("paso7");
             }
         })
     })
@@ -127,6 +156,8 @@ describe('Create members', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 40; index < 43; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test edit name field of a existing member ${index}`);
+
                 cy.get('a[href="#/members/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickMemberLink();
@@ -135,23 +166,30 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso1");
 
                 member.clickFirstMembersListElement();
                 cy.wait(1000);
+                screen("paso2");
 
                 member.clearName();
                 cy.wait(1000);
+                screen("paso3");
 
                 member.typeName(users[index].Displayname);
                 cy.wait(1000);
+                screen("paso4");
 
                 member.saveCreation();
                 cy.wait(2000);
+                screen("paso5");
 
                 member.returnMembersList();
                 cy.wait(1000);
+                screen("paso6");
 
                 member.checkNameInList(users[index].Displayname);
+                screen("paso7");
             }
         })
         
@@ -161,8 +199,13 @@ describe('Create members', () => {
         signIn();
         cy.fixture('wrongEmail.json').then((wrongEmail)=>{
             for (let index = 0; index < 3; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test edit an existing member with a invalid email ${index}`);
+                
                 cy.wait(2000);
-                cy.reload()
+                screen("paso1");
+                
+                cy.reload();
+                screen("paso2");
 
                 cy.get('a[href="#/members/"]').its('length').then((length) => {
                     if (length === 1) {
@@ -172,21 +215,27 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(2000);
+                screen("paso3");
     
                 member.clickFirstMembersListElement();
                 cy.wait(1000);
+                screen("paso4");
     
                 member.clearEmail();
                 cy.wait(1000);
+                screen("paso5");
     
                 member.typeEmail(wrongEmail.value);
                 cy.wait(1000);
+                screen("paso6");
     
                 member.saveCreation();
                 cy.wait(1000);
+                screen("paso7");
                 
                 member.checkErrorMessageExist();
                 cy.wait(1000);
+                screen("paso8");
             }
         })
     });
@@ -195,8 +244,13 @@ describe('Create members', () => {
     it('Test edit an existing member with empty fields', () => {
         signIn();
         for (let index = 0; index < 3; index++) {
+            let screen = screenshot.bind(null, "Add Members", `Test edit an existing member with empty fields ${index}`);
+            
             cy.wait(1000);
+            screen("paso1");
+
             cy.reload();
+            screen("paso2");
 
             cy.get('a[href="#/members/"]').its('length').then((length) => {
                 if (length === 1) {
@@ -206,24 +260,31 @@ describe('Create members', () => {
                 }
             });
             cy.wait(2000);
-            
+            screen("paso3");
+
             member.clickFirstMembersListElement();
             cy.wait(2000);
-            
+            screen("paso3");
+
             member.clearEmail();
             cy.wait(1000);
-            
+            screen("paso4");
+
             member.clearName();
             cy.wait(1000);
-            
+            screen("paso5");
+
             member.clearNote();
             cy.wait(1000);
-            
+            screen("paso6");
+
             member.saveCreation();
             cy.wait(2000);
-            
+            screen("paso7");
+
             member.checkErrorMessageExist();
             cy.wait(2000);
+            screen("paso8");
         }
     });
 
@@ -231,8 +292,13 @@ describe('Create members', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 50; index < 53; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Add member with more fields than allowed in note field ${index}`);
+
                 cy.wait(1000);
+                screen("paso1");
+
                 cy.reload();
+                screen("paso2");
 
                 let memberNote = users[index].Department.repeat(30);
                 
@@ -244,6 +310,7 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso3");
 
                 cy.get('a[href="#/members/new/"]').its('length').then((length) => {
                     if (length === 1) {
@@ -253,25 +320,33 @@ describe('Create members', () => {
                     }
                 });	
                 cy.wait(3000);
+                screen("paso4");
                 
                 member.typeName(users[index].Displayname);
                 cy.wait(1000);
+                screen("paso5");
                 
                 member.typeEmail(users[index].Username);
                 cy.wait(1000);
-                
+                screen("paso6");
+
                 member.typeNote(memberNote);
                 cy.wait(1000);
-                
+                screen("paso7");
+
                 member.saveCreation();
                 cy.wait(2000);
-                
+                screen("paso8");
+
                 member.checkErrorMessageExist();
                 cy.wait(2000);
+                screen("paso9");
 
                 member.clickFirstMemberLink();
-                
+                screen("paso10");
+
                 member.clickLeave();
+                screen("paso11");
             }
         })
         
@@ -281,8 +356,13 @@ describe('Create members', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 26; index < 29; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test edit an existing member with exceded note field ${index}`);
+                
                 cy.wait(1000);
+                screen("paso1");
+
                 cy.reload();
+                screen("paso2");
 
                 let memberNote = users[index].Department.repeat(30);
                 
@@ -294,32 +374,42 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso2");
 
                 member.clickFirstMembersListElement();
                 cy.wait(3000);
-                
+                screen("paso3");
+
                 member.clearName();
                 cy.wait(1000);
+                screen("paso4");
 
                 member.typeName(users[index].Displayname);
                 cy.wait(1000);
+                screen("paso5");
 
                 member.clearEmail();
                 cy.wait(1000);
+                screen("paso6");
 
                 member.typeEmail(users[index].Username);
                 cy.wait(1000);
+                screen("paso7");
                 
                 member.clearNote();
+                screen("paso8");
 
                 member.typeNote(memberNote);
                 cy.wait(1000);
+                screen("paso9");
                 
                 member.saveCreation();
                 cy.wait(2000);
+                screen("paso10");
                 
                 member.checkErrorMessageExist();
                 cy.wait(2000);
+                screen("paso11");
             }
         })
     });
@@ -328,6 +418,8 @@ describe('Create members', () => {
         signIn();
         cy.fixture('users.json').then((users)=>{
             for (let index = 14; index < 17; index++) {
+                let screen = screenshot.bind(null, "Add Members", `Test create member without subscritpion ${index}`);
+                
                 cy.get('a[href="#/members/"]').its('length').then((length) => {
                     if (length === 1) {
                         member.clickMemberLink();
@@ -336,6 +428,7 @@ describe('Create members', () => {
                     }
                 });
                 cy.wait(1000);
+                screen("paso1");
 
                 cy.get('a[href="#/members/new/"]').its('length').then((length) => {
                     if (length === 1) {
@@ -345,26 +438,34 @@ describe('Create members', () => {
                     }
                 });	
                 cy.wait(3000);
+                screen("paso2");
                 
                 member.typeName(users[index].Displayname);
                 cy.wait(1000);
+                screen("paso3");
                 
                 member.typeEmail(users[index].Username);
                 cy.wait(1000);
-                
+                screen("paso4");
+
                 member.typeNote(users[index].Department);
                 cy.wait(1000);
+                screen("paso5");
 
                 member.uncheckSubscribe();
                 cy.wait(1000);
-                
+                screen("paso6");
+
                 member.saveCreation();
                 cy.wait(2000);
-                
+                screen("paso7");
+
                 member.returnMembersList();
                 cy.wait(1000);
-                
+                screen("paso8");
+
                 member.checkNameInList(users[index].Displayname);
+                screen("paso9");
             }
         })
     })
